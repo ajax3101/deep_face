c28adf875c6cf526e5681ec392457938a46befc6
Denys Morozov <ajax3101@gmail.com>
2022-05-23 19:34:55 +0300

    add json file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from deepface import DeepFace
-
+import json
 
 def face_verify(img_1, img_2):
     try:
         result_dict = DeepFace.verify(img1_path=img_1, img2_path=img_2)
+        
+        with open('result.json', 'w') as file:
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
         return result_dict
 
     except Exception as ex:
