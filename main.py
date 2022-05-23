5b817e6f39adf2caff734dccc32f0116fad675d6
Denys Morozov <ajax3101@gmail.com>
2022-05-23 20:47:17 +0300

    add func face_analyse

--- main.py
+++ main.py
@@ -32,12 +32,22 @@ def face_recogn():
 def face_analyse():
     try:
         result_dict = DeepFace.analyze(
-            img_path='faces/mask1.jpg', actions=['age'])
+            img_path='faces/mask1.jpg', actions=['emotion', 'age', 'gender', 'race'])
 
         with open('face_analyse.json', 'w') as file:
             json.dump(result_dict, file, indent=4, ensure_ascii=False)
-            return result_dict
+        print(f'[+] Age: {result_dict.get("age")}')
+        print(f'[+] Gender: {result_dict.get("gender")}')
+        print('[+] Race:')
 
+        for k, v in result_dict.get('race').items():
+            print(f'{k} - {round(v, 2)}%')
+        print('[+] Emotion:')
+
+        for k, v in result_dict.get('emotion').items():
+            print(f'{k} - {round(v, 2)}%')
+            
+        
     except Exception as ex:
         return ex
 
@@ -45,8 +55,8 @@ def face_analyse():
 def main():
     #print(face_verify(img_1='faces/mask1.jpg', img_2='faces/bezos_mask.jpg'))
     # print(face_recogn())
-    print(face_analyse())
-
+    #print(face_analyse())
+    face_analyse()
 
 if __name__ == '__main__':
     main()
