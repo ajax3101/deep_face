04bf31b91295b0b0f6b4d0ad01826d1ffc064560
Denys Morozov <ajax3101@gmail.com>
2022-05-23 19:41:30 +0300

    verified false

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ def face_verify(img_1, img_2):
         
         with open('result.json', 'w') as file:
             json.dump(result_dict, file, indent=4, ensure_ascii=False)
-        return result_dict
+
+        #return result_dict
+        if result_dict.get("verified"):
+            return 'Проверка пройдена! Все отлично!'
+        return 'Алярм, Алярм! Нарушитель!'
 
     except Exception as ex:
         return ex
 
 
 def main():
-   print(face_verify(img_1='faces/mask1.jpg', img_2='faces/mask2.jpg'))
+   print(face_verify(img_1='faces/mask1.jpg', img_2='faces/bezos1.jpg'))
 
 
 if __name__ == '__main__':
