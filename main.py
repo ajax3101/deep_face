75c3140a71a1705a468f0595b2e75861e8ee8937
Denys Morozov <ajax3101@gmail.com>
2022-05-23 20:09:15 +0300

    face_recogn

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from unittest import result
 from deepface import DeepFace
 import json
+
 
 def face_verify(img_1, img_2):
     try:
         result_dict = DeepFace.verify(img1_path=img_1, img2_path=img_2)
-        
+
         with open('result.json', 'w') as file:
             json.dump(result_dict, file, indent=4, ensure_ascii=False)
 
-        #return result_dict
+        # return result_dict
         if result_dict.get("verified"):
             return 'Проверка пройдена! Все отлично!'
         return 'Алярм, Алярм! Нарушитель!'
@@ -17,8 +19,19 @@ def face_verify(img_1, img_2):
         return ex
 
 
+def face_recogn():
+    try:
+        result = DeepFace.find(img_path='faces\mask1.jpg', db_path='ilona')
+        
+        return result
+
+    except Exception as ex:
+        return ex
+
+
 def main():
-   print(face_verify(img_1='faces/mask1.jpg', img_2='faces/bezos1.jpg'))
+   #print(face_verify(img_1='faces/mask1.jpg', img_2='faces/bezos_mask.jpg'))
+    print(face_recogn())
 
 
 if __name__ == '__main__':
