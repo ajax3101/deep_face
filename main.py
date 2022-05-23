36c309d542813022d1ded8ca037fb9b62d13d2de
Denys Morozov <ajax3101@gmail.com>
2022-05-23 19:26:42 +0300

    first commint

---
+++ main.py
@@ -0,0 +1,18 @@
+from deepface import DeepFace
+
+
+def face_verify(img_1, img_2):
+    try:
+        result_dict = DeepFace.verify(img1_path=img_1, img2_path=img_2)
+        return result_dict
+
+    except Exception as ex:
+        return ex
+
+
+def main():
+    face_verify(img_1='faces/mask1.jpg', img_2='faces/mask2.jpg')
+
+
+if __name__ == '__main__':
+    main()
