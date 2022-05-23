68618a1aa960656d6af4a9ec70abef2ec36be66f
Denys Morozov <ajax3101@gmail.com>
2022-05-23 20:17:05 +0300

    face_analyse

--- main.py
+++ main.py
@@ -22,16 +22,30 @@ def face_verify(img_1, img_2):
 def face_recogn():
     try:
         result = DeepFace.find(img_path='faces\mask1.jpg', db_path='ilona')
-        
+        result = result.values.tolist()
         return result
 
     except Exception as ex:
         return ex
 
 
+def face_analyse():
+    try:
+        result_dict = DeepFace.analyze(
+            img_path='faces/mask1.jpg', actions=['age'])
+
+        with open('face_analyse.json', 'w') as file:
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
+            return result_dict
+
+    except Exception as ex:
+        return ex
+
+
 def main():
-   #print(face_verify(img_1='faces/mask1.jpg', img_2='faces/bezos_mask.jpg'))
-    print(face_recogn())
+    #print(face_verify(img_1='faces/mask1.jpg', img_2='faces/bezos_mask.jpg'))
+    # print(face_recogn())
+    print(face_analyse())
 
 
 if __name__ == '__main__':
